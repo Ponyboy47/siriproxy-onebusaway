674d201362b1737eff6f7e5343351eeaff904e07
Ponyboy47 <ponyboy47@gmail.com>
2012-04-04 09:59:11 -0500

    Fixed speech listener

--- lib/siriproxy-onebusaway.rb
+++ lib/siriproxy-onebusaway.rb
@@ -13,7 +13,7 @@ end
 # find stops nearby a given latitude and longitude
 #   - returns an array of Onebusaway::Stop instances
 ##stops = Onebusaway.stops_for_location(:lat => "47.66", :lon => "-122.29")
-	listen_for /Nearest(? bus)? stop/i do
+	listen_for /Nearest(?: bus)? stop/i do
 		say "Latitude is: #{location.latitude}"
 		say "Longitude is: #{location.longitude}"
 	request_completed
@@ -22,7 +22,7 @@ end
 # find routes nearby a given latitude and longitude
 #   - returns an array of Onebusaway::Route instances
 ##routes = Onebusaway.routes_for_location(:lat => "47.66", :lon => "-122.29")
-	listen_for /Nearest(? bus)? route/i do
+	listen_for /Nearest(?: bus)? route/i do
 		say "Latitude is: #{location.latitude}"
 		say "Longitude is: #{location.longitude}"
 	request_completed
