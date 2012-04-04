b0665132ed88344833bad2c032e8bf4bef8c7c3d
Jacob Williams <ponyboy47@gmail.com>
2012-04-04 18:09:43 -0500

    Added actual code since I have an API key

--- lib/siriproxy-onebusaway.rb
+++ lib/siriproxy-onebusaway.rb
@@ -8,6 +8,7 @@ require 'onebusaway_updated'
 
 class SiriProxy::Plugin::OneBusAway < SiriProxy::Plugin
 def initialize(config)
+	Onebusaway.api_key = '74a221d0-feaa-44ea-9518-552ad8665102'
 end
 
 # find stops nearby a given latitude and longitude
@@ -16,6 +17,8 @@ end
 	listen_for /Nearest(?: bus)? stop/i do
 		say "Latitude is: #{location.latitude}"
 		say "Longitude is: #{location.longitude}"
+		stops = Onebusaway.stops_for_location(:lat => "#{location.latitude}", :lon => "#{location.longitude}")
+		say "The nearest bus stop is at #{stops[0]}."
 	request_completed
 	end
 	
@@ -25,6 +28,8 @@ end
 	listen_for /Nearest(?: bus)? route/i do
 		say "Latitude is: #{location.latitude}"
 		say "Longitude is: #{location.longitude}"
+		routes = Onebusaway.routes_for_location(:lat => "#{location.latitude}", :lon => "#{location.longitude}")
+		say "The nearest bus route is #{routes[0]}."
 	request_completed
 	end
 
